46b04572f82c281531c1203e2113b0c6383cac70
Aayush Regmi <aayushreg10@gmail.com>
2022-07-29 02:22:37 +0545

    Optimize,Debug And Complete Game

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ function getPlayerChoice(){
         if (playerSelection=== "rock" || playerSelection==="paper" || playerSelection==="scissors"){
             return playerSelection;
         }else{
-            alert("please select between rock paper and scissors")
+            alert("please select between rock, paper and scissors");
+            getPlayerChoice();
+            
         }
     
 }
@@ -24,8 +26,7 @@ function getPlayerChoice(){
 function singleRound(playerSelection,computerSelection){
     if(playerSelection==="rock" && computerSelection==="paper"){
         console.log("You lose paper beats rock!!");
-        return result="computer wins"
-        
+        return result="computer wins";
     
     }else if(playerSelection==="rock" && computerSelection==="rock"){
         console.log("Its a draw, you both got rocked!");
@@ -34,21 +35,27 @@ function singleRound(playerSelection,computerSelection){
     }else if(playerSelection==="rock" && computerSelection==="scissors"){
         console.log("Congratulaions You win! Rock beats scissors.");
         return result="player wins";
+    
     }else if(playerSelection==="paper"&& computerSelection==="rock"){
         console.log("Congratulaions You win! Paper beats rock.");
         return result="player wins";
+    
     }else if(playerSelection==="paper" && computerSelection==="scissors"){
         console.log("You lose scissors beat paper.");
         return result="computer wins";
+    
     }else if(playerSelection==="paper"&& computerSelection==="paper"){
         console.log("Both paper, Its a draw");
         return result="draw";
+    
     }else if(playerSelection==="scissors"&& computerSelection==="scissors"){
         console.log("Both scissors, Its a draw");
         return result="draw";
+    
     }else if(playerSelection==="scissors"&& computerSelection==="paper"){
         console.log("Congratulations you win! Scissors beats paper.");
         return result="player wins";
+    
     }else if(playerSelection==="scissors"&& computerSelection==="rock"){
         console.log("You lose rock beats scissors");
         return result="computer wins";
@@ -60,6 +67,7 @@ function singleRound(playerSelection,computerSelection){
 function fiveGames(){
     let playerScore=0;
     let compScore=0;
+    let drawScore=0;
     
     for(let i=0;i<5;i++){
         const playerSelection=getPlayerChoice();
@@ -67,24 +75,47 @@ function fiveGames(){
         singleRound(playerSelection,computerSelection);
         if (result=="player wins"){
             playerScore++;
-            console.log("Player wins and score is "+playerScore);
+            console.log("You win and score is "+playerScore+'-'+compScore);
         }else if (result=="computer wins"){
             compScore++;
-            console.log("computer wins and score is "+compScore);
+            console.log("computer wins and score is "+compScore+'-'+playerScore);
 
         }else{
-            console.log("its a draw")
+            drawScore++;
+            console.log("its a draw the score is "+playerScore+"-"+compScore);
         }
 
-           
-               
-            
         }
         if (playerScore>compScore){
-            console.log(`YAY you won with score ${playerScore}-${compScore}`);
+            console.log(`YAY you won with score ${playerScore}-${compScore} with ${drawScore} number of draws.` );
+            response_one=prompt("You won!! Wanna play again?");
+            response_one=response_one.toLowerCase();
+            if(response_one==="yes"){
+                fiveGames();
+            }else{
+                alert("Thankyou for playing");
+            }
+        
+        }else if(playerScore===compScore){
+            console.log(`It was a draw with score ${playerScore}-${compScore} and ${drawScore} number of draws. Wanna try again?`);
+            response_two=prompt("You Drew!! Wanna play again?");
+            response_two=response_two.toLowerCase();
+            if(response_two==="yes"){
+                fiveGames();
+            }else{
+                alert("Thankyou for playing");
+            }
+            
         }
         else{
-            'play again'
+            response_three=prompt("You lost!! Wanna play again?");
+            response_three=response_three.toLowerCase();
+            if(response_three==="yes"){
+                fiveGames();
+            }else{
+                alert("Thankyou for playing");
+            }
+
            
     }
     
